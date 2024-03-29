ba76d52d2537874924b2c84986309a380d1b5510
emirhanusta <emirhan1usta@gmail.com>
2024-03-29 03:09:33 +0300

    added Dockerfiles and docker-compose file

--- backend/src/test/java/earthquake/mapping/EarthquakeMappingApplicationTests.java
+++ backend/src/test/java/earthquake/mapping/EarthquakeMappingApplicationTests.java
@@ -1,13 +1,9 @@
 package earthquake.mapping;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EarthquakeMappingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
